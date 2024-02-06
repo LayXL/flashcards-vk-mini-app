746cda97c30aaaa64268ad48e096f87494a69ac7
Виталий <46228376+LayXL@users.noreply.github.com>
2024-02-06 13:55:22 +0500

    removed api

--- packages/client/src/panels/home.tsx
+++ packages/client/src/panels/home.tsx
@@ -1,13 +1,8 @@
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router"
 import { PanelHeader, Paragraph, Button } from "@vkontakte/vkui"
-import { trpc } from "../shared/api"
 
 export const Home = () => {
     const routeNavigator = useRouteNavigator()
-
-    const { isLoading } = trpc.healthCheck.useQuery()
-
-    if (isLoading) return
 
     return (
         <>
