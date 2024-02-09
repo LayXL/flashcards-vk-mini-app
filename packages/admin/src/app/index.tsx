44557ffce169ac4c42048ac6be984028d4b170c4
Виталий <46228376+LayXL@users.noreply.github.com>
2024-02-09 23:58:13 +0500

    moved app index file

--- packages/admin/src/app/index.tsx
+++ packages/admin/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Words } from "./pages/words"
+import { Words } from "../pages/words"
 import "./app/globals.css"
 
 createRoot(document.getElementById("root")!).render(
