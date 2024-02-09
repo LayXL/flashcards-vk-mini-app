b8e1439fcacfdd8f5bcde71b4e138f58cbfea9be
Виталий <46228376+LayXL@users.noreply.github.com>
2024-02-09 23:56:15 +0500

    first admin page

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Words } from "./pages/words"
-import "./main.css"
+import "./app/globals.css"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- packages/admin/src/pages/words.tsx
+++ packages/admin/src/pages/words.tsx
@@ -1,3 +1,101 @@
+import {
+    Table,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
+} from "@/components/ui/table"
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+
+type Word = {
+    id: number
+    language: string
+    original: string
+    foreign: string
+    usageExample: string
+}
+
+const payments: Word[] = [
+    {
+        id: 1,
+        language: "Russian",
+        original: "Туристический маршрут",
+        foreign: "Tourist route",
+        usageExample:
+            "The tourist route takes you through the heart of the city, passing by all the major landmarks and attractions.",
+    },
+]
+
+const columns: ColumnDef<Word>[] = [
+    {
+        accessorKey: "language",
+        header: "Язык",
+    },
+    {
+        accessorKey: "original",
+        header: "Оригинал",
+    },
+    {
+        accessorKey: "foreign",
+        header: "На иностранном",
+    },
+    {
+        accessorKey: "usageExample",
+        header: "Пример использования",
+    },
+]
+
 export const Words = () => {
-    return <>123</>
+    const table = useReactTable({
+        data: payments,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+        <div className="rounded-xl border m-4">
+            <Table>
+                <TableHeader
+                    children={table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow
+                            key={headerGroup.id}
+                            children={headerGroup.headers.map((header) => (
+                                <TableHead
+                                    key={header.id}
+                                    children={
+                                        header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef.header,
+                                                  header.getContext(),
+                                              )
+                                    }
+                                />
+                            ))}
+                        />
+                    ))}
+                />
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
