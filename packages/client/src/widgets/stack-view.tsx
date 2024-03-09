cecea8ad8b716824e7a76cb4e6fca8b536a388bc
PraporPingvin <sofasybbotina12@gmail.com>
2024-03-10 00:44:05 +0500

    создала screen

--- packages/client/src/widgets/stack-view.tsx
+++ packages/client/src/widgets/stack-view.tsx
@@ -1,8 +1,25 @@
-import { CellButton, Div, ModalPageHeader, PanelHeaderBack } from "@vkontakte/vkui"
+import {
+    Button,
+    ButtonGroup,
+    CellButton,
+    Div,
+    Header,
+    IconButton,
+    ModalPageHeader,
+    PanelHeaderBack,
+    Subhead,
+} from "@vkontakte/vkui"
 import { Fragment } from "react"
 import { TranslationCard } from "../entities/translation/ui/translation-card"
 import { useModal } from "../features/modal/contexts/modal-context"
 import { trpc } from "../shared/api"
+import {
+    Icon24Add,
+    Icon24LikeOutline,
+    Icon24MoreHorizontal,
+    Icon28MoreHorizontal,
+} from "@vkontakte/icons"
+import { FeedTranslationCard } from "../entities/translation/ui/feed-translation-card"
 
 type StackViewProps = {
     id: number
@@ -16,14 +33,60 @@ export const StackView = ({ id }: StackViewProps) => {
         onSuccess: () => refetch(),
     })
 
+    const {mutate: addReaction} = trpc.stacks.addReaction.useMutation()
+
     return (
         <>
-            <ModalPageHeader
+            <ModalPageHeader className="z-20"
                 before={<PanelHeaderBack onClick={() => modal?.onClose()} />}
                 children={data?.name}
             />
 
-            {data?.description && <Div>{data?.description}</Div>}
+            <div className="flex flex-col relative ">
+                <div className="absolute bg-blue-300 w-[360px] aspect-square -z-1 left-1/2 -translate-x-1/2 opacity-50 -top-[260px] rounded-full blur-3xl"></div>
+                <div className="flex flex-row items-center justify-center gap-6 py-3 z-20">
+                    <div className="rounded-full p-2.5 bg-vk-default text-accent">
+                        <Icon24MoreHorizontal />
+                    </div>
+                    <div className="w-[200px] aspect-square bg-vk-default rounded-xl"></div>
+                    <div className="rounded-full p-2.5 bg-vk-default text-accent" onClick={()=> addReaction({stackId:id})}>
+                        <Icon24LikeOutline />
+                    </div>
+                </div>
+                <Div>
+                    <Subhead className="text-center text-secondary ">{data?.description}</Subhead>
+                </Div>
+                <Div>
+                    <ButtonGroup stretched>
+                        <Button mode="primary" stretched size="l">
+                            Играть
+                        </Button>
+                        <Button mode="secondary" stretched size="l" before={<Icon24Add />}>
+                            Дублировать
+                        </Button>
+                    </ButtonGroup>
+                </Div>
+                <Header
+                    mode="primary"
+                    aside={
+                        <IconButton className="text-accent">
+                            <Icon24Add />
+                        </IconButton>
+                    }
+                >
+                    Переводы
+                </Header>
+                <Div className="grid grid-cols-cards gap-3">
+                    {data?.translations.map(({ translation }) => (
+                        <FeedTranslationCard
+                            foreign={translation.foreign}
+                            vernacular={translation.vernacular}
+                        />
+                    ))}
+                </Div>
+            </div>
+
+            {/* {data?.description && <Div>{data?.description}</Div>}
 
             <Div>
                 {data?.translations.map(({ translation }) => (
@@ -46,7 +109,7 @@ export const StackView = ({ id }: StackViewProps) => {
                         />
                     </Fragment>
                 ))}
-            </Div>
+            </Div> */}
         </>
     )
 }
