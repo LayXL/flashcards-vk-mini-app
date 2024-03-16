ee87beea6fbc822132035ed65acd2f132de86392
Виталий <46228376+LayXL@users.noreply.github.com>
2024-03-16 11:59:41 +0500

    improvements

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from "vite"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        sentryVitePlugin({
+            org: "vlay",
+            project: "flashcards-react",
+        }),
+    ],
     server: {
         proxy: {
             "/api": "http://localhost:3000",
         },
+    },
+    build: {
+        sourcemap: true,
     },
 })
