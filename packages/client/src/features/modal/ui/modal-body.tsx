4698ad61a8dbb7e3a88ba94af11b040fa262030a
Виталий <46228376+LayXL@users.noreply.github.com>
2024-03-18 20:41:09 +0500

    fix

--- packages/client/src/features/modal/ui/modal-body.tsx
+++ packages/client/src/features/modal/ui/modal-body.tsx
@@ -17,7 +17,7 @@ export const ModalBody = ({ children, fullscreen = false }: ModalBodyProps) => {
                 "overflow-scroll overscroll-none",
                 "rounded-t-2xl bg-vk-content",
                 "max-h-screen max-w-[480px] w-full",
-                "pb-[env(safe-area-inset-bottom)] mx-auto m-t-[env(safe-area-inset-top)]",
+                "pb-[env(safe-area-inset-bottom)] mx-auto mt-[env(safe-area-inset-top)]",
                 fullscreen && "h-screen",
             )}
             children={children}
