188ca28e10417b5976322c344cf97c7afa6b7d4b
Виталий <46228376+LayXL@users.noreply.github.com>
2024-04-02 19:39:48 +0500

    fix

--- packages/client/src/widgets/leaderboard.tsx
+++ packages/client/src/widgets/leaderboard.tsx
@@ -1,5 +1,12 @@
 import bridge from "@vkontakte/vk-bridge"
-import { ModalPageHeader, PanelHeaderBack, Tabs, TabsItem } from "@vkontakte/vkui"
+import {
+    Button,
+    ModalPageHeader,
+    PanelHeaderBack,
+    Placeholder,
+    Tabs,
+    TabsItem,
+} from "@vkontakte/vkui"
 import { useEffect, useState } from "react"
 import { RatingUserCard } from "../entities/rating/ui/rating-user-card"
 import { trpc } from "../shared/api"
@@ -12,6 +19,7 @@ type LeaderboardProps = {
 export const Leaderboard = ({ onClose }: LeaderboardProps) => {
     const [tab, setTab] = useState<"friends" | "global">("friends")
     const [frinedsIds, setFriendsIds] = useState<number[] | null>(null)
+    const [hasAccessToFriends, setHasAccessToFriends] = useState<boolean | null>(null)
 
     // TODO придумать что выводить, если нет друзей, играющие в этот сезон
 
@@ -21,8 +29,6 @@ export const Leaderboard = ({ onClose }: LeaderboardProps) => {
     const { data: currentUser } = trpc.getUser.useQuery()
 
     useEffect(() => {
-        if (tab !== "friends") return
-
         bridge
             .send("VKWebAppGetAuthToken", {
                 app_id: 51843841,
@@ -44,8 +50,10 @@ export const Leaderboard = ({ onClose }: LeaderboardProps) => {
                         if (!data.response?.items) return
                         setFriendsIds(data.response.items)
                     })
+                    .catch(() => setHasAccessToFriends(false))
             })
-    }, [currentUser?.vkId, tab])
+            .catch(() => setHasAccessToFriends(false))
+    }, [currentUser?.vkId])
 
     return (
         <>
@@ -77,6 +85,47 @@ export const Leaderboard = ({ onClose }: LeaderboardProps) => {
                     isCurrentUser={user.id === currentUser?.id}
                 />
             ))}
+
+            {hasAccessToFriends === false && tab === "friends" && (
+                <Placeholder
+                    header={"Мы не знаем кто ваши друзья"}
+                    children={
+                        "Пожалуйста, разрешите доступ к друзьям, чтобы посмотреть таблицу лидеров среди друзей"
+                    }
+                    action={
+                        <Button
+                            onClick={() => {
+                                bridge
+                                    .send("VKWebAppGetAuthToken", {
+                                        app_id: 51843841,
+                                        scope: "friends",
+                                    })
+                                    .then((data) => {
+                                        if (!data.access_token) return
+
+                                        bridge
+                                            .send("VKWebAppCallAPIMethod", {
+                                                method: "friends.get",
+                                                params: {
+                                                    user_id: parseInt(currentUser?.vkId ?? "0"),
+                                                    v: "5.131",
+                                                    access_token: data.access_token,
+                                                },
+                                            })
+                                            .then((data) => {
+                                                if (!data.response?.items) return
+                                                setFriendsIds(data.response.items)
+                                            })
+                                            .catch(() => setHasAccessToFriends(false))
+                                    })
+                                    .catch(() => setHasAccessToFriends(false))
+                            }}
+                            children={"Разрешить"}
+                            size={"l"}
+                        />
+                    }
+                />
+            )}
         </>
     )
 }
