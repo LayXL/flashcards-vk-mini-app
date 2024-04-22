1dde7991357efceb1f509fa6ed087c0872a02ed5
Виталий <46228376+LayXL@users.noreply.github.com>
2024-04-22 23:11:52 +0500

    fix

--- packages/client/src/entities/story/content/subscribe-to-community.tsx
+++ packages/client/src/entities/story/content/subscribe-to-community.tsx
@@ -10,7 +10,7 @@ export const SubscribeToCommunityStory = () => {
                 initial={{ scale: 1 }}
                 animate={{ scale: 1.1 }}
                 transition={{ duration: 30, easings: ["easeInOut"] }}
-                className={"w-full h-full absolute -z-10"}
+                className={"w-full h-full absolute z-10 pointer-events-none touch-none"}
             >
                 <motion.img
                     animate={controls}
@@ -29,7 +29,7 @@ export const SubscribeToCommunityStory = () => {
                     }}
                 />
             </motion.div>
-            <div className={"flex flex-col font-vk-sans"}>
+            <div className={"flex flex-col font-vk-sans z-20 pointer-events-none touch-none"}>
                 <div
                     style={{
                         height: "var(--vkui_internal--panel_header_height)",
@@ -69,7 +69,7 @@ export const SubscribeToCommunityStory = () => {
                     </motion.p>
                 </Div>
             </div>
-            <Div className={"absolute bottom-0 w-full box-border mb-safe-area-bottom"}>
+            <Div className={"absolute bottom-0 w-full box-border mb-safe-area-bottom z-20"}>
                 <Button
                     className={"!bg-white !text-[#0077FF]"}
                     children={"Подписаться"}
