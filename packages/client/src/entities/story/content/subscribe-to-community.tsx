e9dfd64bf6356721f56d8bec240c7deec0a777d8
Виталий <46228376+LayXL@users.noreply.github.com>
2024-04-24 19:37:06 +0500

    fix

--- packages/client/src/entities/story/content/subscribe-to-community.tsx
+++ packages/client/src/entities/story/content/subscribe-to-community.tsx
@@ -5,7 +5,7 @@ export const SubscribeToCommunityStory = () => {
     const controls = useAnimationControls()
 
     return (
-        <div className={"bg-[#FF3B73] flex justify-between relative text-white"}>
+        <div className={"bg-[#FF3B73] relative text-white"}>
             <motion.div
                 initial={{ scale: 1 }}
                 animate={{ scale: 1.1 }}
@@ -29,56 +29,63 @@ export const SubscribeToCommunityStory = () => {
                     }}
                 />
             </motion.div>
-            <div className={"flex flex-col font-vk-sans z-20 pointer-events-none touch-none"}>
-                <div
-                    style={{
-                        height: "var(--vkui_internal--panel_header_height)",
-                    }}
-                />
-                <Div
-                    className={"w-full py-9 box-border gap-4 flex-col flex"}
-                    style={{
-                        paddingLeft: "calc(var(--vkui--size_base_padding_vertical--regular) * 2)",
-                    }}
-                >
-                    <motion.p
-                        transition={{
-                            delay: 0.3,
-                            duration: 0.5,
-                            type: "spring",
-                            bounce: 0.15,
+            <div
+                className={
+                    "flex flex-col font-vk-sans z-20 pointer-events-none touch-none justify-between"
+                }
+            >
+                <div>
+                    <div
+                        style={{
+                            height: "var(--vkui_internal--panel_header_height)",
                         }}
-                        initial={{ translateX: "-100%" }}
-                        animate={{ translateX: "0%" }}
-                        className={"text-4xl font-bold"}
-                    >
-                        Вступай в&nbsp;сообщество
-                    </motion.p>
-                    <motion.p
-                        transition={{
-                            delay: 0.5,
-                            duration: 0.5,
-                            type: "spring",
-                            bounce: 0.15,
+                    />
+                    <Div
+                        className={"w-full py-9 box-border gap-4 flex-col flex"}
+                        style={{
+                            paddingLeft:
+                                "calc(var(--vkui--size_base_padding_vertical--regular) * 2)",
                         }}
-                        initial={{ translateX: "-100%" }}
-                        animate={{ translateX: "0%" }}
-                        className={"text-xl font-medium"}
                     >
-                        И узнавай о&nbsp;новых возможностях в&nbsp;приложении
-                    </motion.p>
+                        <motion.p
+                            transition={{
+                                delay: 0.3,
+                                duration: 0.5,
+                                type: "spring",
+                                bounce: 0.15,
+                            }}
+                            initial={{ translateX: "-100%" }}
+                            animate={{ translateX: "0%" }}
+                            className={"text-4xl font-bold"}
+                        >
+                            Вступай в&nbsp;сообщество
+                        </motion.p>
+                        <motion.p
+                            transition={{
+                                delay: 0.5,
+                                duration: 0.5,
+                                type: "spring",
+                                bounce: 0.15,
+                            }}
+                            initial={{ translateX: "-100%" }}
+                            animate={{ translateX: "0%" }}
+                            className={"text-xl font-medium"}
+                        >
+                            И узнавай о&nbsp;новых возможностях в&nbsp;приложении
+                        </motion.p>
+                    </Div>
+                </div>
+                <Div>
+                    <Button
+                        className={"!bg-white !text-[#0077FF]"}
+                        children={"Подписаться"}
+                        stretched
+                        size={"l"}
+                        href={"https://vk.com/learning_app"}
+                        target={"_blank"}
+                    />
                 </Div>
             </div>
-            <Div className={"absolute bottom-0 w-full box-border mb-safe-area-bottom z-20"}>
-                <Button
-                    className={"!bg-white !text-[#0077FF]"}
-                    children={"Подписаться"}
-                    stretched
-                    size={"l"}
-                    href={"https://vk.com/learning_app"}
-                    target={"_blank"}
-                />
-            </Div>
         </div>
     )
 }
