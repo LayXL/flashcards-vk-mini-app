35d886b475b9953fb8231d092703345d79b24e10
Виталий <46228376+LayXL@users.noreply.github.com>
2024-04-29 13:49:59 +0500

    Changed Tab bar

--- packages/client/src/features/tab-bar/lib/tabs.ts
+++ packages/client/src/features/tab-bar/lib/tabs.ts
@@ -1,35 +1,33 @@
 import {
-    Icon28Cards2Outline,
     Icon28HieroglyphCharacterOutline,
-    Icon28HomeOutline,
     Icon28PlayCircle,
     Icon28UserCircleOutline,
 } from "@vkontakte/icons"
 
 export const tabs = [
+    // {
+    //     label: "Главная",
+    //     icon: Icon28HomeOutline,
+    //     view: "main",
+    //     url: "/",
+    // },
+    // {
+    //     label: "Стопки",
+    //     icon: Icon28Cards2Outline,
+    //     view: "stacks",
+    //     url: "/stacks",
+    // },
     {
-        label: "Главная",
-        icon: Icon28HomeOutline,
-        view: "main",
-        url: "/",
-    },
-    {
-        label: "Стопки",
-        icon: Icon28Cards2Outline,
-        view: "stacks",
-        url: "/stacks",
+        label: "Новое",
+        icon: Icon28HieroglyphCharacterOutline,
+        view: "new",
+        url: "/new",
     },
     {
         label: "Играть",
         icon: Icon28PlayCircle,
         view: "play",
         url: "/play",
-    },
-    {
-        label: "Новое",
-        icon: Icon28HieroglyphCharacterOutline,
-        view: "new",
-        url: "/new",
     },
     {
         label: "Профиль",
