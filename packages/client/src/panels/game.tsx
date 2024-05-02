c1df52de5f59f32d1a0e4a18fed3e251331875db
Виталий <46228376+LayXL@users.noreply.github.com>
2024-05-02 14:13:20 +0500

    Improvements

--- packages/client/src/panels/game.tsx
+++ packages/client/src/panels/game.tsx
@@ -1,16 +1,22 @@
 import { PanelHeader } from "@vkontakte/vkui"
 import { ModalWindow } from "../features/modal/ui/modal-window"
 import { TabBar } from "../features/tab-bar/ui/tab-bar"
+import { trpc } from "../shared/api"
 import { useModalState } from "../shared/hooks/useModalState"
 import { Leaderboard } from "../widgets/leaderboard"
 import { SelectGame } from "../widgets/select-game"
 
 export const Game = () => {
+    const { data: dailyStreak } = trpc.stats.getDailyStreak.useQuery()
+
     const ratingModal = useModalState()
 
     return (
         <>
-            <PanelHeader children={"Игровые режимы"} />
+            <PanelHeader
+                children={"Игровые режимы"}
+                before={<span children={dailyStreak?.streakCount ?? ":("} />}
+            />
 
             <SelectGame />
 
