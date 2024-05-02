44dc827bdf29f9510242855fbc3f3540553ffd64
Виталий <46228376+LayXL@users.noreply.github.com>
2024-05-02 14:37:09 +0500

    renamed heading

--- packages/client/src/panels/game.tsx
+++ packages/client/src/panels/game.tsx
@@ -11,7 +11,7 @@ export const Game = () => {
 
     return (
         <>
-            <PanelHeader children={"Игровые режимы"} before={<DailyStreak />} />
+            <PanelHeader children={"Играть"} before={<DailyStreak />} />
 
             <SelectGame />
 
