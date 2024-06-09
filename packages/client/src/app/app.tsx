9e70d264c7640608c655df576441a15292e8428f
Виталий <46228376+LayXL@users.noreply.github.com>
2024-06-09 12:49:19 +0500

    revert

--- packages/client/src/app/app.tsx
+++ packages/client/src/app/app.tsx
@@ -32,30 +32,30 @@ Settings.defaultLocale = "ru"
 
 bridge.send("VKWebAppInit", {})
 
-if (!document.getElementById("ym")) {
-    const script = document.createElement("script")
+// if (!document.getElementById("ym")) {
+//     const script = document.createElement("script")
 
-    script.id = "ym"
+//     script.id = "ym"
 
-    script.innerHTML = `
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-            m[i].l = 1 * new Date();
-            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-        })
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+//     script.innerHTML = `
+//         (function (m, e, t, r, i, k, a) {
+//             m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+//             m[i].l = 1 * new Date();
+//             for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+//             k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+//         })
+//         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(97019311, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-        });
-    `
+//         ym(97019311, "init", {
+//             clickmap: true,
+//             trackLinks: true,
+//             accurateTrackBounce: true,
+//             webvisor: true
+//         });
+//     `
 
-    document.head.appendChild(script)
-}
+//     document.head.appendChild(script)
+// }
 
 export const App = () => {
     const [queryClient] = useState(
