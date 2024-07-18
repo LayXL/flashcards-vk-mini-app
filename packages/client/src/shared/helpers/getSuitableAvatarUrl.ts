217c4dda87279c67e8dbb2c51d00dfd541a6bfcb
Виталий <46228376+LayXL@users.noreply.github.com>
2024-07-18 11:28:48 +0500

    fixes

--- packages/client/src/shared/helpers/getSuitableAvatarUrl.ts
+++ packages/client/src/shared/helpers/getSuitableAvatarUrl.ts
@@ -1,8 +1,12 @@
 export const getSuitableAvatarUrl = (avatarUrls: unknown = {}, preferredSize: number = 100) => {
+    if (avatarUrls === null || avatarUrls === undefined) {
+        return undefined
+    }
+
     const entries = Object.entries(avatarUrls as Record<number | "max", string>)
 
     const preferredAvatar = entries.find(
-        ([size]) => typeof size === "number" && size >= preferredSize,
+        ([size]) => typeof size === "number" && size >= preferredSize
     )
 
     try {
